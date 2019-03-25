b35c1d1c9755cae934afea33472d49abccbe6c5f
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2019-03-24 23:11:13 -0500

    Fixed issue with html5 history-mode and vue-router in Flask

--- app/views.py
+++ app/views.py
@@ -13,10 +13,17 @@ from flask import render_template, request
 ###
 
 
-@app.route('/')
-def index():
-    """Render website's initial page and let VueJS take over."""
-    return render_template('index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    """
+    Because we use HTML5 history mode in vue-router we need to configure our
+    web server to redirect all routes to index.html. Hence the additional route
+    "/<path:path".
+
+    Also we will render the initial webpage and then let VueJS take control.
+    """
+    return app.send_static_file('index.html')
 
 
 # Here we define a function to collect form errors from Flask-WTF
