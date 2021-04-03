35bb9c08cc980164f395cbf7920143f6a6baf71b
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2021-04-03 14:48:25 -0500

    Add configuration file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from app.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+
+app.config_from_object(Config)
 
 from app import views
